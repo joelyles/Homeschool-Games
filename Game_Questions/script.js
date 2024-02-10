aa2915cdcd2074f295f8ef04bbbd341047ffa433
Joe Lyles <lylesjoe@protonmail.com>
2024-02-10 05:28:18 -0500

    continued to build out loop

--- Game_Questions/script.js
+++ Game_Questions/script.js
@@ -8,13 +8,15 @@ const timeBtn = document.querySelector('.time-enter');
 const timeResult = document.querySelector('.c-container');
 const clockBg = document.getElementById('clock-entry');
 
+const gameBox = document.querySelector('.game-container-one');
 const mainContainer = document.querySelector('.m-container');
 
 const clockFaces = [
-    {
+    /* {
         time: `test`,
         picture: `<img src="images/clocks.png"/>`
-    },  {
+    },   */
+    {
         time: `1200`,
         picture: `<img src="images/clocks12.jpg"/>`
     },  {
@@ -23,7 +25,7 @@ const clockFaces = [
     },  {
         time: `430`,
         picture: `<img src="images/clocks430.jpg"/>`
-    },  {
+    },/*   {
         time: `830`,
         picture: `<img src="images/clocks830.jpg"/>`
     },  {
@@ -38,26 +40,51 @@ const clockFaces = [
     }, {
         time: `1100`,
         picture: `<img src="images/clocks11.jpg"/>`
-    }, 
+    },  */
 ];
 
-let random = Math.floor(Math.random() * clockFaces.length);
+/* let random = Math.floor(Math.random() * clockFaces.length);
+clocks.innerHTML = clockFaces[random].picture; */
 
 window.addEventListener('load', function(){
-    clocks.innerHTML = clockFaces[random].picture;
-            /*  */
-    let firstQuestion = clockFaces[0].picture;
+    
     
     for (let i = 0; i < clockFaces.length; i++) {
         let newDiv = document.createElement('div');
-// how to iterate through array sequentially instead of random
-        newDiv.innerHTML = clockFaces[random].picture;
-        mainContainer.append(newDiv);
-        newDiv.classList.add('left-container');
+        let entryBoxDiv = document.createElement('div');
+        let entryInput = document.createElement('input');
+        let submitButton = document.createElement('button');
+
+        newDiv.innerHTML = clockFaces[i].picture;
+        gameBox.append(newDiv);
+        newDiv.classList.add('game-container-one');
+
+        entryBoxDiv.innerHTML = 'type answer below';
+        newDiv.append(entryBoxDiv);
+        entryBoxDiv.classList.add('entry-container');
+
+        entryBoxDiv.append(entryInput);
+        entryInput.classList.add('time-answer');
+
+        entryBoxDiv.append(submitButton);
+        submitButton.innerHTML = 'submit';
+        submitButton.classList.add('time-enter');
+
+
+        submitButton.addEventListener('click', function(){
+            if(entryInput.value === clockFaces[i].time){
+                console.log('it worked!');
+            }
+        });
     }
 });
 
-timeBtn.addEventListener('click', function(){
+
+
+
+
+//
+/* timeBtn.addEventListener('click', function(){
     if(timeInput.value === clockFaces[random].time){
         timeResult.textContent = "correct!";
 
@@ -65,14 +92,19 @@ timeBtn.addEventListener('click', function(){
 
         timeResult.style.color = "green";
         timeResult.style.fontSize = "48px"
-        /* sectionTwo.scrollIntoView({behavior: 'smooth'}); */
+        sectionTwo.scrollIntoView({behavior: 'smooth'});
     } else {
         timeResult.textContent = "sorry, try again."
         timeResult.style.color = "red";
         timeResult.style.fontSize = "48px";
         timeInput.value = "";
     }
-});
+}); */
+//
+
+
+
+
 
 // edited html - need to add button
 /* const reload = document.querySelector('.reload');
