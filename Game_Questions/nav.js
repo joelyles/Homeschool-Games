b1cd1f708228bfc20b388718420ea4163fce91e4
Joe Lyles <lylesjoe@protonmail.com>
2024-02-23 05:25:29 -0500

    started responsive nav bar

---
+++ Game_Questions/nav.js
@@ -0,0 +1,7 @@
+const navPlus = document.querySelector('.nav-plus');
+const navLine = document.querySelector('.nav-line');
+
+navPlus.addEventListener('click', function(){
+    navLine.classList.toggle('bye');
+    
+});
